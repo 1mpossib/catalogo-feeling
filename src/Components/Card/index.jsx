9a9c6b3d742855269edf7f08a11fc07939579089
Ruan Belmiro <ruan.carlos.belmiro@gmail.com>
2025-03-11 20:00:18 -0300

    Fix  slug toUpperCase

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -39,7 +39,7 @@ export default function Card({ data, item }) {
               color: "#CCF7FF",
             }}
           >
-            {slug.toUpperCase()}
+            {slug?.toUpperCase()}
           </Text>
         </Tag>
         <div>
